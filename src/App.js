07f662636bbaa0a3051c7ff525f32e8e242877fe
fscozzatti <fscozzatti@gmail.com>
2020-06-24 22:07:47 -0300

    Despues de la maldita clase 66!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import WeatherLocation from './componets/WeatherLocation';
+import React , { Component } from 'react';
 import './App.css';
+import LocationList from './componets/LocationList';
 
-function App() {
-  return (
-    <div className="App">
-      <WeatherLocation city={"Rosario, ar"}/>
-    </div>
-  );
+const cities = [
+  'Buenos Aires,ar',
+  'Washington,us',
+  'Bogota,col',
+  'Ciudad de México,mx',
+  'Madrid,es',
+  'Lima,pe',
+  'Rosario,ar',
+];
+
+class App extends Component {
+ 
+  handleSelectionLocation = city =>{
+    console.log(`HandleSelectionLocation: ${city}`);
+  }
+ 
+  render(){ 
+    return(<div>
+        <LocationList 
+          cities={cities}
+          onSelectedLocation={this.handleSelectionLocation} 
+        />
+      </div>
+    )
+  }
 }
-
 export default App;
--- src/componets/WeatherLocation/index.js
+++ src/componets/WeatherLocation/index.js
@@ -38,11 +38,12 @@ class WeatherLocation extends Component{
         })
 
     }
-
+    
     render(){
+        const {onWeatherLocationClick} = this.props;
         const { city, data } = this.state;
         return(
-        <div className="weatherLocationCont"> 
+        <div className="weatherLocationCont" onClick={onWeatherLocationClick}> 
             <Location city={city}></Location>
             {data ?
                 <WeatherData data={data}></WeatherData> :
@@ -56,6 +57,7 @@ class WeatherLocation extends Component{
 
 WeatherLocation.propTypes = {
     city: PropTypes.string.isRequired,
+    onWeatherLocationClick: PropTypes.func,
 }
 
 export default WeatherLocation;
